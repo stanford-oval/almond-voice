99f945a877d0f7ee1fcfb0eb743658a0a9c6abee
Euirim Choi <euirim@me.com>
2019-11-15 23:20:23 -0800

    Add settings and begin index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+// tslint:disable:no-expression-statement
+import dotenv from 'dotenv'
+// initialize dotenv
+dotenv.config();
+
+import debug from 'debug'; // tslint:disable-line
+
+// Debug init
+// const micDebug = debug('mic');
+// const recognizerDebug = debug('recognizer');
+debug.enable('mic recognizer');
+
+const main = async () => {
+  // trigger stt
+  // query almond
+  // trigger tts
+  return;
+};
+
+main();
