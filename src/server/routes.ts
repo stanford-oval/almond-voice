144f40c101439966c7d939d9d8b36358b83ae34e
Euirim Choi <euirim@me.com>
2020-02-04 22:01:41 -0800

    Create better rest api endpoint for stt

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,58 @@
 import express from 'express';
+import multer from 'multer';
+import {
+  AudioInputStream,
+  ResultReason,
+  SpeechRecognitionResult,
+} from '@euirim/microsoft-cognitiveservices-speech-sdk';
+import { initRecognizer } from '../lib/csr';
+import { bufferToStream, toArrayBuffer } from '../utils/buffer';
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage }); // audio kept in-memory
 const router = express.Router();
+
+router.post('/rest/stt', upload.single('audio'), (req, res, next) => {
+  const sdkAudioInputStream = AudioInputStream.createPushStream();
+  const recognizer = initRecognizer(sdkAudioInputStream);
+
+  recognizer.recognized = (_, e) => {
+    // Indicates that recognizable speech was not detected, and that recognition is done.
+    if (e.result.reason === ResultReason.NoMatch) {
+      res.status(400).json({
+        success: false,
+        error: 'Speech unrecognizable.',
+      });
+    }
+  };
+
+  recognizer.recognizeOnceAsync(
+    (result: SpeechRecognitionResult) => {
+      console.log(`Result: ${result.text}`);
+      res.status(200).json({
+        success: true,
+        text: result.text,
+      });
+      recognizer.close();
+    },
+    () => {
+      res.status(400).json({
+        success: true,
+        error: 'Speech recognition failed due to internal error.',
+      });
+      recognizer.close();
+    },
+  );
+
+  const memoryStream = bufferToStream(req.file.buffer);
+  memoryStream
+    .on('data', (data: any) => {
+      console.log('Received data!');
+      sdkAudioInputStream.write(toArrayBuffer(data));
+    })
+    .on('end', () => {
+      sdkAudioInputStream.close();
+    });
+});
 
 export default router;
--- src/utils/buffer.ts
+++ src/utils/buffer.ts
@@ -1,3 +1,7 @@
+import stream from 'stream';
+
+const { Duplex } = stream;
+
 /* Adapted from https://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer */
 const toArrayBuffer = (buf: Buffer): ArrayBuffer => {
   const ab = new ArrayBuffer(buf.length);
@@ -8,5 +12,12 @@ const toArrayBuffer = (buf: Buffer): ArrayBuffer => {
   return ab;
 };
 
-// eslint-disable-next-line import/prefer-default-export
-export { toArrayBuffer };
+// Adapted from https://stackoverflow.com/questions/46975590/how-to-upload-file-saved-in-memory-by-multer-to-another-api
+const bufferToStream = (buffer: Buffer): stream.Duplex => {
+  const duplexStream = new Duplex();
+  duplexStream.push(buffer);
+  duplexStream.push(null);
+  return duplexStream;
+};
+
+export { toArrayBuffer, bufferToStream };
